cabee90cf4350036547e8bd65ccfbddd3eb6191b
AlaaBaravi <97338784+AlaaBaravi@users.noreply.github.com>
2026-02-08 17:37:32 +0200

    Update webhooks.shop.redact.jsx

--- delifast/app/routes/webhooks.shop.redact.jsx
+++ delifast/app/routes/webhooks.shop.redact.jsx
@@ -2,5 +2,5 @@ import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }) => {
   await authenticate.webhook(request);
-  return new Response("OK", { status: 200 });
+  return new Response(null, { status: 200 });
 };
