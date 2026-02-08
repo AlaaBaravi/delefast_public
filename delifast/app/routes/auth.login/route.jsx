91a24d9163a0806858cf34badf71bd62082fb24c
AlaaBaravi <97338784+AlaaBaravi@users.noreply.github.com>
2026-02-08 19:34:47 +0200

    Update route.jsx

--- delifast/app/routes/auth.login/route.jsx
+++ delifast/app/routes/auth.login/route.jsx
@@ -2,7 +2,6 @@ import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { useState } from "react";
 import { Form, useActionData, useLoaderData } from "react-router";
 import { login } from "../../shopify.server";
-import { loginErrorMessage } from "./error";
 
 export const loader = async ({ request }) => {
   const errors = loginErrorMessage(await login(request));
