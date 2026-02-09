672b529c9e861d1cdb573dc5ab317b56462dfe51
AlaaBaravi <97338784+AlaaBaravi@users.noreply.github.com>
2026-02-09 14:13:23 +0200

    Update webhooks.app.scopes_update.jsx

--- delifast/app/routes/webhooks.app.scopes_update.jsx
+++ delifast/app/routes/webhooks.app.scopes_update.jsx
@@ -1,7 +1,8 @@
 import { verifyWebhook } from "../webhooks.verify.server";
 
 export const action = async ({ request }) => {
-  const v = await verifyShopifyWebhook(request);
+  const v = await verifyWebhook(request);
+
   if (!v.ok) return new Response("Unauthorized", { status: 401 });
 
   return new Response("OK", { status: 200 });
