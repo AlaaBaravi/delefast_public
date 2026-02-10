00b75e2d36e4812745deedfc1b4e8bb225eac5cf
AlaaBaravi <97338784+AlaaBaravi@users.noreply.github.com>
2026-02-10 14:29:16 +0200

    Update route.jsx

--- delifast/app/routes/auth.login/route.jsx
+++ delifast/app/routes/auth.login/route.jsx
@@ -1,46 +1,44 @@
-import { AppProvider } from "@shopify/shopify-app-react-router/react";
-import { useState } from "react";
-import { Form, useActionData, useLoaderData } from "react-router";
+// app/routes/auth.login/route.jsx
+
+import { redirect, Form, useLoaderData } from "react-router";
 import { login } from "../../shopify.server";
+import styles from "./styles.module.css";
 
+// Loader to handle the login logic and redirection
 export const loader = async ({ request }) => {
-  const errors = loginErrorMessage(await login(request));
+  const url = new URL(request.url);
 
-  return { errors };
+  // Redirect if shop domain is provided
+  if (url.searchParams.get("shop")) {
+    throw redirect(`/app?${url.searchParams.toString()}`);
+  }
+
+  return { showForm: Boolean(login) };
 };
 
-export const action = async ({ request }) => {
-  const errors = loginErrorMessage(await login(request));
-
-  return {
-    errors,
-  };
-};
-
-export default function Auth() {
-  const loaderData = useLoaderData();
-  const actionData = useActionData();
-  const [shop, setShop] = useState("");
-  const { errors } = actionData || loaderData;
+export default function App() {
+  const { showForm } = useLoaderData();
 
   return (
-    <AppProvider embedded={false}>
-      <s-page>
-        <Form method="post">
-          <s-section heading="Log in">
-            <s-text-field
-              name="shop"
-              label="Shop domain"
-              details="example.myshopify.com"
-              value={shop}
-              onChange={(e) => setShop(e.currentTarget.value)}
-              autocomplete="on"
-              error={errors.shop}
-            ></s-text-field>
-            <s-button type="submit">Log in</s-button>
-          </s-section>
-        </Form>
-      </s-page>
-    </AppProvider>
+    <div className={styles.index}>
+      <div className={styles.content}>
+        <h1 className={styles.heading}>Welcome to [your app]</h1>
+        <p className={styles.text}>
+          Log in to your Shopify store and get started.
+        </p>
+        {showForm && (
+          <Form className={styles.form} method="post" action="/auth/login">
+            <label className={styles.label}>
+              <span>Shop domain</span>
+              <input className={styles.input} type="text" name="shop" />
+              <span>e.g: my-shop-domain.myshopify.com</span>
+            </label>
+            <button className={styles.button} type="submit">
+              Log in
+            </button>
+          </Form>
+        )}
+      </div>
+    </div>
   );
 }
