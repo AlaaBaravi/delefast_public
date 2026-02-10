7c03309aec66fb7000d55af534e21c1d2556ece5
AlaaBaravi <97338784+AlaaBaravi@users.noreply.github.com>
2026-02-10 14:26:44 +0200

    Update webhooks.verify.server.js

--- delifast/app/webhooks.verify.server.js
+++ delifast/app/webhooks.verify.server.js
@@ -2,11 +2,13 @@
 
 import crypto from 'crypto';
 
+// Function to verify the Shopify webhook using HMAC
 export const verifyShopifyWebhook = async (request) => {
   const hmacHeader = request.headers.get('X-Shopify-Hmac-Sha256');
-  const body = await request.text();  // Get raw body content of the request
-  const secret = process.env.SHOPIFY_API_SECRET_KEY;  // Set your secret key
+  const body = await request.text();
+  const secret = process.env.SHOPIFY_API_SECRET_KEY;  // Your secret key
 
+  // Generate the HMAC hash from the request body and compare with the header
   const hash = crypto
     .createHmac('sha256', secret)
     .update(body, 'utf8')
